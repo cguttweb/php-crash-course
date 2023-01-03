f86b8e57ca916abcd82206192e85bbc068064ceb
Chloe <cguttweb@pm.me>
2023-01-03 18:59:26 +0000

    learning basics of sanitizing input and cookies

--- 10_get_post.php
+++ 10_get_post.php
@@ -4,3 +4,26 @@
 /*
   We can pass data through urls and forms using the $_GET and $_POST superglobals.
 */
+// echo $_GET['name'];
+// echo $_GET['age'];
+if (isset($_POST['submit'])) {
+  echo $_POST['name'];
+  echo $_POST['age'];
+}
+?>
+
+<a href="<?php echo $_SERVER['PHP_SELF'] ?>?name=Chloe&age=30">Click</a>
+
+<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+  <div>
+    <label>Name:
+      <input type="text" name="name">
+    </label>
+  </div>
+  <div>
+    <label>Age:
+      <input type="text" name="age">
+    </label>
+  </div>
+  <input type="submit" value="Submit" name="submit">
+</form>
--- 11_sanitizing_inputs.php
+++ 11_sanitizing_inputs.php
@@ -4,3 +4,30 @@
 /*
   Data submitted through a form is not sanitized by default. We have methods to sanitize data manually.
 */
+
+if (isset($_POST['submit'])) {
+  $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
+  $age = filter_input(INPUT_POST, 'age', FILTER_SANITIZE_SPECIAL_CHARS);
+  // $name = htmlspecialchars($_POST['name']);
+  // $age = htmlspecialchars($_POST['age']);
+
+  // $name = filter_var($_POST['name'], FILTER_SANITIZE_SPECIAL_CHARS);
+  echo $name;
+}
+?>
+
+<a href="<?php echo $_SERVER['PHP_SELF'] ?>?name=Chloe&age=30">Click</a>
+
+<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+  <div>
+    <label>Name:
+      <input type="text" name="name">
+    </label>
+  </div>
+  <div>
+    <label>Age:
+      <input type="text" name="age">
+    </label>
+  </div>
+  <input type="submit" value="Submit" name="submit">
+</form>
