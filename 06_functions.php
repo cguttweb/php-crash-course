3a1a7f416f2ae6c3e6973b32197e719ab923b199
Chloe <cguttridge@clever-touch.com>
2023-01-03 14:37:42 +0000

    notes on different fucntions

--- 06_functions.php
+++ 06_functions.php
@@ -12,3 +12,32 @@
     // code to be executed
   }
 */
+
+function registerUser($email)
+{
+  $x = 10; // function scoped
+  echo $email . ' registered';
+}
+
+// echo $x; // throws error
+// registerUser('Chloe'); // needs to have an argument passed in
+// can pass in default values
+function sum($num1 = 4, $num2 = 6)
+{
+  return $num1 + $num2;
+}
+
+// echo sum();
+
+// anonymous functions
+
+$subtract = function ($num1, $num2) {
+  return $num1 - $num2;
+};
+
+// echo $subtract(254, 134);
+
+// can use arrow functions e.g.
+
+$multiply = fn ($num1, $num2) => $num1 * $num2;
+echo $multiply(9, 18);
--- 07_array_functions.php
+++ 07_array_functions.php
@@ -5,3 +5,69 @@
   Functions to work with arrays
   https://www.php.net/manual/en/ref.array.php
 */
+
+$vegetables = ['potatoes', 'onions', 'carrots'];
+
+// get length
+echo count($vegetables);
+
+// search in array returns boolean
+// var_dump(in_array('onions', $vegetables));
+
+// add new values to array
+
+$vegetables[] = 'sprouts';
+// add to start of array
+array_unshift($vegetables, 'parsnips');
+// adds to end of array
+array_push($vegetables, 'cabbages', 'lettuces');
+
+// remove values from an array
+// remove from start of array
+array_shift($vegetables);
+// remove from end
+array_pop($vegetables);
+// remove specific element
+// unset($vegetables[3]);
+
+// split into chunks
+$array_chunks = array_chunk($vegetables, 2);
+// print_r($array_chunks);
+
+// print_r($vegetables);
+$arr1 = ['10', '11', '12'];
+$arr2 = ['20', '21', '22'];
+$new_arr = array_merge($arr1, $arr2);
+// or using spread operator
+$new_arr2 = [...$arr1, ...$arr2];
+// print_r($new_arr);
+
+$colors = ['red', 'green', 'yellow'];
+$fruits = ['apple', 'avocado', 'banana'];
+// colors = keys, fruits = values
+$combined_arr = array_combine($colors, $fruits);
+// print_r($combined_arr);
+
+$keys = array_keys($combined_arr);
+// print_r($keys);
+
+$flip_arr = array_flip($combined_arr);
+// flips keys and values
+// print_r($flip_arr);
+
+$nums = range(1, 20);
+// print_r($nums);
+// similar to JS map
+$newNums = array_map(function ($number) {
+  return "Number ${number}";
+}, $nums);
+
+// print_r($newNums);
+
+$lessThan7 = array_filter($nums, fn ($number) => $number <= 7);
+
+// print_r($lessThan7);
+
+$sum = array_reduce($nums, fn ($carry, $number) => $carry + $number);
+
+// var_dump($sum);
